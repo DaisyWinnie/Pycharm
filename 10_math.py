d4698f4978a57f5afc456f4b0102f1d38f2f0f8a
DaisyWinnie <daisy_winnie@163.com>
2025-05-08 12:07:20 +0800

    math

---
+++ 10_math.py
@@ -0,0 +1,13 @@
+import math
+a = -1
+b = -2
+c = 3
+print((-b + math.sqrt(b ** 2 - 4 * a * c))/ (2 * a))
+print((-b - math.sqrt(b ** 2 - 4 * a * c))/ (2 * a))
+# 乘方：**
+# 运算顺序：括号（）；平方**； 乘除* /；加减=-
+# 2的三次方：2 ** 3
+# print为内置函数，math是一个专门提供数学运算函数的库，导入方法是在文件最开始一行输入import math
+# math sin(1)
+# sqrt返回平方根
+# Mac使用“command” + “/”可以多行加注释
